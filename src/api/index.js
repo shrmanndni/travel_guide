bcbb1ef193c5c600d0ffdb875264d43e0e15f554
Nandini Sharma <sharmanandini1677@gmail.com>
2022-07-05 01:29:04 +0530

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+          "x-rapidapi-key": '076aa2a038mshab1e4675b65046ep12cde1jsn195d59099692',
+        },
+      }
+    );
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    const { data } = await axios.get(
+      "https://community-open-weather-map.p.rapidapi.com/find",
+      {
+        params: {
+          lon: lng,
+          lat: lat,
+        },
+        headers: {
+          "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+          "x-rapidapi-key": '076aa2a038mshab1e4675b65046ep12cde1jsn195d59099692',
+        },
+      }
+    );
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
